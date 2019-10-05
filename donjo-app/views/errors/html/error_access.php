df9a33d3919487ef58c3951f1c187840eebfc04c
fusionid <zeppyshiddiq@gmail.com>
2019-10-05 13:16:46 +0700

    Penambahan halaman forbidden access

---
+++ donjo-app/views/errors/html/error_access.php
@@ -0,0 +1,20 @@
+<div class="content-wrapper">
+	<section class='content-header'>
+		<h1></h1>
+		
+	</section>
+	<section class='content'>
+		<div class='row'>
+			<div class='col-xs-12'>
+				<div class='box box-danger'>
+					<div class='box-body'>
+
+                                                <center><img src="<?php echo base_url()?>assets/images/403-forbidden.png" alt="403"/></center>
+						<center><h4>Anda tidak mempunyai <b>HAK AKSES</b> untuk halaman ini</h4></center>
+                                                <center><h5>Harap hubungi Administrator OpenSID di desa anda</h5></center>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</div>
