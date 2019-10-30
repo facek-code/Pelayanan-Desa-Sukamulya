c455cb72c53ad002ca10b7a2b535f50f8415a897
fusionid <zeppyshiddiq@gmail.com>
2019-10-30 14:44:09 +0700

    Migrasi Otomatis DB yg sudah ada ke DB RBAC

--- donjo-app/config/ion_auth.php
+++ donjo-app/config/ion_auth.php
@@ -42,6 +42,7 @@ $config['tables']['users']           = 'users';
 $config['tables']['groups']          = 'groups';
 $config['tables']['users_groups']    = 'users_groups';
 $config['tables']['login_attempts']  = 'login_attempts';
+$config['tables']['group_perm']      = 'group_perm';
 
 /*
  | Users table column and Group table column you want to join WITH.
